af7850dc467a74ecce1b97c1bb52ea3a4c68da3a
sandeesh <sandeeshr@techprovint.com>
2023-03-12 09:46:35 +0530

    Setup google login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from 'react';
+
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
+import jwt_decode from "jwt-decode";
 
 const Login = () => {
-  return (
-    <div> Login </div>
-  )
+    const responseGoogle = (response) => {
+        console.log(response.credential);
+        var decoded = jwt_decode(response.credential);
+        localStorage.setItem('user', JSON.stringify(decoded));
+        const{ name, email, picture} = decoded;
+
+        const doc = {
+            _id: email,
+            _type: 'user',
+            userName: name,
+            image: picture,
+        }
+    }
+
+    return (
+        <div className='flex justify-start items-center flex-col h-screen'>
+            <div className='relative w-full h-full'>
+                <video
+                    src={shareVideo}
+                    type="video/mp4"
+                    loop
+                    controls={false}
+                    muted
+                    autoPlay
+                    className='w-full h-full object-cover'
+                />
+                <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+                    <div className='p-5'>
+                        <img src={logo} width='130px' alt='logo' />
+                    </div>
+                    <div className='shadow-2x1'>
+                        <GoogleOAuthProvider clientId= {process.env.REACT_APP_GOOGLE_API_TOKEN}>
+                            <GoogleLogin
+                                render={(renderProps) => (
+                                    <button
+                                        type='button'
+                                        className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'
+                                        onClick={renderProps.onClick}
+                                        disabled={renderProps.disabled}
+                                    >
+                                        <FcGoogle className='mr-4' /> Sign in with google
+                                    </button>
+                                )}
+                                onSuccess={responseGoogle}
+                                onFailure={responseGoogle}
+                                cookiePolicy="single_host_origin"
+                            />
+                        </GoogleOAuthProvider>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Login
